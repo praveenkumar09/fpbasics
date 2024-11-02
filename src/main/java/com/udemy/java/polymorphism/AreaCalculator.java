d93bb7a00027d72557f2cd287a2837afd8f14d4d
praveenkumar09 <tpkprav@gmail.com>
2024-11-02 23:28:37 +0800

    Basics for datatype anbd poly added

---
+++ src/main/java/com/udemy/java/polymorphism/AreaCalculator.java
@@ -0,0 +1,5 @@
+package com.udemy.java.polymorphism;
+
+public class AreaCalculator {
+
+}
---
+++ src/main/java/come/udemy/java/datatype/PrimitiveType.java
@@ -0,0 +1,14 @@
+package come.udemy.java.datatype;
+
+public class PrimitiveType {
+    public static void main(String[] args) {
+        int i = 5;
+        System.out.println("i = " + i);
+        change(i);
+        System.out.println("i = " + i);
+    }
+
+    private static void change(int i) {
+        i++;
+    }
+}
