085bd13954ebcf530d90622686ea824232be5671
praveenkumar09 <tpkprav@gmail.com>
2024-11-03 10:10:37 +0800

    Basics for interface added

--- src/main/java/com/udemy/java/interfaces/TestInterface.java
+++ src/main/java/com/udemy/java/interfaces/TestInterface.java
@@ -3,6 +3,9 @@ package com.udemy.java.interfaces;
 public class TestInterface {
     public static void main(String[] args) {
         Alarm phone = new IPhone();
+        Alarm alexa = () -> System.out.println("Alexa setting alarm");
+        testSetAlarm(alexa);
+        testSetAlarm(phone);
     }
 
     public static void testSetAlarm(Alarm alarm){
