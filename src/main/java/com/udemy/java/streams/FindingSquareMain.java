2488f4d7dbe83989598be42e16ab6de55b922653
praveenkumar09 <tpkprav@gmail.com>
2024-11-09 08:58:39 +0800

    Finding square example added

---
+++ src/main/java/com/udemy/java/streams/FindingSquareMain.java
@@ -0,0 +1,37 @@
+package com.udemy.java.streams;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FindingSquareMain {
+    private static final Set<Integer> numbersSet = new HashSet<>();
+
+    public static void main(String[] args) {
+        numbersSet.add(1);
+        numbersSet.add(2);
+        numbersSet.add(3);
+        numbersSet.add(4);
+        numbersSet.add(5);
+        numbersSet.add(6);
+        numbersSet.add(7);
+        numbersSet.add(8);
+        numbersSet.add(9);
+
+        /*for(Integer number : numbersSet) {
+            if(number % 2 ==0)
+                System.out.println("the number is :"+number * number);
+            else
+                System.out.println("the number is :"+number);
+        }*/
+
+        //Streams
+        numbersSet
+                .stream()
+                .filter(i -> i % 2 == 0)
+                .limit(3)
+                .map(i -> i * i)
+                .forEach(System.out::println);
+
+
+    }
+}
