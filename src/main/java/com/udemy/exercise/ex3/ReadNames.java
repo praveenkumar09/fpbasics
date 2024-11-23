5be704026cef28a7095872fab5d1c2ec5a71ac48
praveenkumar09 <tpkprav@gmail.com>
2024-11-23 19:35:28 +0800

    Assignments files and sols added

--- src/main/java/com/udemy/exercise/ex3/ReadNames.java
+++ src/main/java/com/udemy/exercise/ex3/ReadNames.java
@@ -14,19 +14,19 @@ public class ReadNames {
     public static void main(String[] args) throws IOException {
         List<String> firstNameList = Files.readAllLines(Path.of("/Users/praveenkumarthangaraj/Documents/fp/module/basics/fpbasics/src/main/java/com/udemy/exercise/ex3/first-names.txt"));
         //** ex-1 solution **//
-        long countOfNamesStartsWithB = firstNameList
-                .parallelStream()
-                .filter(name -> name.startsWith("B"))
-                .count();
+        long countOfNamesStartsWithB = firstNameList.parallelStream().filter(name -> name.startsWith("B")).count();
         System.out.println(countOfNamesStartsWithB);
         //** ex-2 solution **//
-        ArrayList<String> nameListFromC = firstNameList
-                .parallelStream()
-                .filter(name -> name.startsWith("C") &&
-                        name.contains("s"))
-                .collect(toCollection(
-                        ArrayList::new
-                ));
+        ArrayList<String> nameListFromC = firstNameList.parallelStream().filter(name -> name.startsWith("C") && name.contains("s")).collect(toCollection(ArrayList::new));
         System.out.println(nameListFromC);
+        //* ex-3 solution **//
+        long countOfChars = firstNameList
+                .parallelStream()
+                .filter(name -> name.startsWith("M"))
+                .flatMap((name) -> name.chars().boxed()
+                )
+                .count();
+        System.out.println(countOfChars);
+
     }
 }
