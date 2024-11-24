5275a1fd0d438cc79fa404b3af8e4af119819610
praveenkumar09 <tpkprav@gmail.com>
2024-11-24 19:14:38 +0800

    Added examples for var inb lambda variables

--- src/main/java/com/udemy/java10/TypeInferenceWithVar.java
+++ src/main/java/com/udemy/java10/TypeInferenceWithVar.java
@@ -2,6 +2,8 @@ package com.udemy.java10;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 public class TypeInferenceWithVar {
     public static void main(String[] args) {
@@ -14,7 +16,14 @@ public class TypeInferenceWithVar {
         System.out.println(number);
         //var a; not possible
         //var a = "praveen" is possible
+        test(4,5);
 
 
+    }
+
+    public static void test(int a, int b){
+        //cannot use var here in lambda functions
+        BiConsumer<Integer,Integer> doubleIt = (x,y) -> System.out.println(x*y);
+        doubleIt.accept(a,b);
     }
 }
